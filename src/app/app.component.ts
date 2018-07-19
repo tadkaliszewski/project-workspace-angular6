f91b30f0a483d93399c05e5f482660242ff0baf3
Michał <michal.jabi@gmail.com>
2018-07-19 10:25:26 +0200

    feature: datamodel and new type to menu, switching sites using ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import {Component} from '@angular/core';
 
+type MenuType = 'projects' | 'top-ten' | 'blogs';
+
+interface MenuItem {
+  type: MenuType;
+  title: string;
+}
+
 @Component({
   selector: 'pw-root' ,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Project Workspace';
+
+  menuItems: MenuItem[] = [
+    { title: 'Projects', type: 'projects'},
+    { title: 'Top 10', type: 'top-ten'},
+    { title: 'Technical Blog', type: 'blogs'}
+  ];
+  chosenSite: MenuType = 'blogs';
+
+  handleMenuClick(site: MenuType) {
+    this.chosenSite = site;
+  }
 }
