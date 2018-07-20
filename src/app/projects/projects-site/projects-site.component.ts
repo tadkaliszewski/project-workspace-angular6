7f5ef518947e45f44f13261efc97c6a2bc927b71
Michał <michal.jabi@gmail.com>
2018-07-20 14:50:43 +0200

    fix: leak in subscriptions - close them via takeUntil Operator

--- src/app/projects/projects-site/projects-site.component.ts
+++ src/app/projects/projects-site/projects-site.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Project} from '../Project';
 import {ProjectsService} from '../projects.service';
+import {Subject} from 'rxjs';
+import {takeUntil, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'pw-projects-site',
   templateUrl: './projects-site.component.html',
   styleUrls: ['./projects-site.component.css']
 })
-export class ProjectsSiteComponent implements OnInit {
+export class ProjectsSiteComponent implements OnInit, OnDestroy {
 
   searchTerm = '';
   // nowDate = new Date();
   projectList: Project[] = [];
+
+  unsubStream = new Subject<void>();
+
   constructor(private projectsService: ProjectsService) {}
 
   ngOnInit() {
     this.projectsService
       .getAllProjects()
+      .pipe(
+        tap((project: Project[]) => console.log('Twoje projekty', project)),
+        takeUntil(this.unsubStream)
+      )
       .subscribe((project: Project[]) => {
-        console.log('Twoje projekty', project);
         this.projectList = [...project];
       });
+
     console.log('ProjectsSiteComponent ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ProjectsSiteComponent ngOnDestroy');
+    this.unsubStream.next();
+    this.unsubStream.complete();
   }
 
 }
